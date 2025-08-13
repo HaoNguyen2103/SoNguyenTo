eb54127cb5ee47412a1e7a8433f55dfe32408753
HaoNguyen2103 <nguyenhao21031995@gmail.com>
2025-08-13 23:11:44 +0700

    upload
    Bài tập lọc ra các số nguyên tố

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿class Program 
+{
+    static void Main(string[] args)
+    {
+        int number = 0;
+       Console.WriteLine("Cac so nguyen to nho hon 100 la:");
+        for (int i = 2; i <= 100; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                if (i % j == 0)
+                {
+                    number++;
+                }
+            }
+            if (number == 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
